925c94e9b7d47de1afce054486b5a2b9b7539bc4
Kali Donovan <kali@deviantech.com>
2012-01-11 13:12:02 +0700

    Cleanup

--- lib/apn/tasks.rb
+++ lib/apn/tasks.rb
@@ -8,16 +8,9 @@ namespace :apn do
   task :sender => :setup do
     require 'apn'
 
-    worker = nil
-
-    begin
-      worker = APN::Sender.new(:full_cert_path => ENV['FULL_CERT_PATH'], :cert_path => ENV['CERT_PATH'], :environment => ENV['ENVIRONMENT'], :cert_pass => ENV['CERT_PASS'])
-      worker.verbose = ENV['LOGGING'] || ENV['VERBOSE']
-      worker.very_verbose = ENV['VVERBOSE']
-    rescue Exception => e
-      raise e
-      # abort "set QUEUE env var, e.g. $ QUEUE=critical,high rake resque:work"
-    end
+    worker = APN::Sender.new(:full_cert_path => ENV['FULL_CERT_PATH'], :cert_path => ENV['CERT_PATH'], :environment => ENV['ENVIRONMENT'], :cert_pass => ENV['CERT_PASS'])
+    worker.verbose = ENV['LOGGING'] || ENV['VERBOSE']
+    worker.very_verbose = ENV['VVERBOSE']
 
     puts "*** Starting worker to send apple notifications in the background from #{worker}"
 
