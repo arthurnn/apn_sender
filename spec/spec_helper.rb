d3f8a9b854aa543a2f52edb843969a79c91f04dd
Arthur Neves <arthurnn@gmail.com>
2013-05-03 22:59:55 -0400

    removing test/* and add rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+
+require "apn"
+require "rspec"
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+end
