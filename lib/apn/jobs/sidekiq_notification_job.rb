a64cc45bfc7fe788cc9c446f507bc1e3ccdd0cb8
Tiago Franco <gama.franco@gmail.com>
2013-10-13 19:27:20 +0100

    Fixes typo on sidekiq worker.

--- lib/apn/jobs/sidekiq_notification_job.rb
+++ lib/apn/jobs/sidekiq_notification_job.rb
@@ -1,6 +1,6 @@
 module APN::Jobs
-  # This is the class that's actually enqueued via Resque when user calls +APN.notify+.
-  # It gets added to the +apple_server_notifications+ Resque queue, which should only be operated on by
+  # This is the class that's actually enqueued via Sidekiq when user calls +APN.notify+.
+  # It gets added to the +apple_server_notifications+ Sidekiq queue, which should only be operated on by
   # workers of the +APN::Sender+ class.
   class SidekiqNotificationJob
     include Sidekiq::Worker
